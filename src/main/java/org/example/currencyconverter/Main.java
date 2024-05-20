504e33fa6fd0cb59df22b0608ffc19fcf26cf8e7
ArunVasunny <arunvasunny153@gmail.com>
2024-05-20 18:51:57 +0530

    added Mousedrag and pressed for making the window movable

--- src/main/java/org/example/currencyconverter/Main.java
+++ src/main/java/org/example/currencyconverter/Main.java
@@ -1,19 +1,47 @@
 package org.example.currencyconverter;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application
 {
+    //For Making window draggable
+    private double x = 0;
+    private double y = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("HomeUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 680, 720);
+
+        Parent root = FXMLLoader.load(getClass().getResource("HomeUI.fxml"));
+        Scene scene = new Scene(root,680, 720);
         scene.getStylesheets().add(getClass().getResource("HomeUI.css").toExternalForm());
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                x = event.getSceneX();
+                y = event.getSceneY();
+                System.out.println("Mouse pressed X="+x+ " Y="+y);
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - x);
+                stage.setY(event.getScreenY() - y);
+            }
+        });
+
         stage.setTitle("Currency Converter");
         stage.setResizable(false);
         stage.setScene(scene);
