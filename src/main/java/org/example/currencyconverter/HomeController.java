5a8c25be2f341bf7c5b42c17c6daea7cd722e26e
ArunVasunny <arunvasunny153@gmail.com>
2024-05-20 12:11:51 +0530

    declared component vars

--- src/main/java/org/example/currencyconverter/HomeController.java
+++ src/main/java/org/example/currencyconverter/HomeController.java
@@ -1,14 +1,38 @@
 package org.example.currencyconverter;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HomeController {
     @FXML
-    private Label welcomeText;
+    private AnchorPane BgAnchor;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private AnchorPane WhitePane;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Label label4;
+
+    @FXML
+    private Label label5;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private Button result;
+
+
 }
