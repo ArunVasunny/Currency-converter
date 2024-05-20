4dd3f11618efefff6bf95b943dcd7e30ad87db2b
ArunVasunny <arunvasunny153@gmail.com>
2024-05-20 12:13:06 +0530

    assign css

--- src/main/java/org/example/currencyconverter/Main.java
+++ src/main/java/org/example/currencyconverter/Main.java
@@ -13,7 +13,9 @@ public class Main extends Application
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("HomeUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 680, 720);
+        scene.getStylesheets().add(getClass().getResource("HomeUI.css").toExternalForm());
         stage.setTitle("Currency Converter");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
