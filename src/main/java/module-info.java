b786b737ba4aafa956122b71d87c4cb220c4e4e9
ArunVasunny <arunvasunny153@gmail.com>
2024-05-23 15:48:12 +0530

    not necessary

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.currencyconverter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
+    requires org.json;
 
 
     opens org.example.currencyconverter to javafx.fxml;
