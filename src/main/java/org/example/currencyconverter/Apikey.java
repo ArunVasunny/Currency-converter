319c5588bad28d1f9b4be5095295c92f6c44319b
ArunVasunny <arunvasunny153@gmail.com>
2024-05-23 15:31:29 +0530

    File storing api key

---
+++ src/main/java/org/example/currencyconverter/Apikey.java
@@ -0,0 +1,7 @@
+package org.example.currencyconverter;
+
+public class Apikey {
+    public static final String apiKey = "4XbsLoxVkFOAK5kqnQtwBhiYr7zYryST";
+//    public static final String apiKey = "vNGS74Nzc078u0Svy0dXYPUcop3zmWWw";
+
+}
